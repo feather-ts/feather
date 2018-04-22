cdc295377a99955d3b6eefe63064fb6472026683
Andreas Prudzilko <mendrik76@gmail.com>
2018-04-22 21:39:53 +0300

    add rollup back

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@
 <img src="https://travis-ci.org/feather-ts/feather.svg?branch=master" width="90" height="20">
 
 Find more information from the [official website](http://www.feather-ts.com)
+
+### Requirements
+
+`npm install parcel-bundler parcel-plugin-typescript -g`
--- src/typescript/feather.ts
+++ src/typescript/feather.ts
@@ -15,3 +15,23 @@ import './decorators/inject'
 import './decorators/template-node'
 import './core/bind'
 import './core/cleanup'
+
+export {isDef, isUndef, isFunction, compose, throttle} from './utils/functions'
+export {observeArray, ArrayListener, range, removeFromArray} from './utils/arrays'
+export {
+    WidgetClass, ensure, addPropertyListener, Callback, deepValue, getSubset, isObject, merge,
+    PropertyCallback, TypedMap
+} from './utils/objects'
+export {allChildNodes, allTextNodes} from './utils/dom'
+export {Computed} from './decorators/computed'
+export {start, AnyWidget, ArrayWidget, addToConstructorQueue, Construct, ConstructConf, Widget} from './decorators/construct'
+export {EventConfig, On, Scope} from './decorators/event'
+export {Delete, FetchConfig, Get, Post, Processor, Put} from './decorators/fetch'
+export {Storable, LocalStorage} from './decorators/local-storage'
+export {MediaQuery} from './decorators/media-query'
+export {navigate, runRoutes} from './decorators/router'
+export {Transformer} from './decorators/transformer'
+export {Inject} from './decorators/inject'
+export {TemplateNode} from './decorators/template-node'
+export {findWidget, findWidgets, render} from './core/bind'
+export {registerCleanUp} from './core/cleanup'
