86b7ef239025397884aa3460b373c9d9d5352b57
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-04 13:43:02 +0300

    remove logs

--- src/typescript/decorators/template-node.ts
+++ src/typescript/decorators/template-node.ts
@@ -2,7 +2,6 @@ import {addToRenderQueue, AnyWidget, EnhancedConstructor} from './construct'
 
 export const TemplateNode = (selector: string) => (proto: AnyWidget, property: string) => {
     addToRenderQueue(proto.constructor as EnhancedConstructor, (widget, node) => {
-        console.log(node.outerHTML, selector, property)
         widget[property] = node.querySelector(selector)
     })
 }
